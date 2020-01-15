edd3504cd73f455503e440d2f788ccc8d0fc1202
Ka Lung Sy <kalungsy@gmail.com>
2020-01-15 14:26:13 -0800

    add Stripe webhooks handler for create customer event

--- public/class-servicebot-public.php
+++ public/class-servicebot-public.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once 'stripe/init.php';
+use Stripe\Stripe;
+use Stripe\Event;
+use Stripe\Webhook;
+
 /**
  * The public-facing functionality of the plugin.
  *
@@ -120,13 +125,13 @@ function servicebot_ajax_create_user() {
 	
 	//On success
 	if ( ! is_wp_error( $user_id ) ) {
+		wp_new_user_notification( $user_id, null, 'both');
 		wp_send_json( array(    'user_id' => $user_id,
 								'email' => $email,
 								'name' => $name,
 								'password' => '*****',
 								'message' => 'User created successfully.'
 					), 200 );
-		wp_new_user_notification( $user_id, null, 'both');
 	}else{
 		wp_send_json_error( array(  'email' => $email,
 									'name' => $name,
@@ -135,6 +140,85 @@ function servicebot_ajax_create_user() {
 							), 500 );
 	}
 
-  }
-  add_action( 'wp_ajax_create_user', 'servicebot_ajax_create_user' );
-  add_action( 'wp_ajax_nopriv_create_user', 'servicebot_ajax_create_user' );
+}
+add_action( 'wp_ajax_create_user', 'servicebot_ajax_create_user' );
+add_action( 'wp_ajax_nopriv_create_user', 'servicebot_ajax_create_user' );
+
+
+
+function servicebot_webhook_listener() {
+
+	register_rest_route( 'servicebot/v1', '/stripe-hooks', array(
+		'methods'  => 'POST'
+	  ) );
+
+	if ( $_SERVER['REQUEST_URI'] === '/servicebot/v1/stripe-hooks'){
+
+		Stripe::setApiKey('sk_test_Xf2X48WTwXe3SWgYS5codx0e00OMX5n6Ze');
+		$endpoint_secret = 'whsec_ENSrf2tDZZX7KXMMwxgVcCDDscAc8y75';
+		
+		$payload = @file_get_contents( 'php://input' );
+		$sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+		$event = null;
+		
+		try {
+			$event = Webhook::constructEvent(
+				$payload, $sig_header, $endpoint_secret
+			);
+		} catch(\UnexpectedValueException $e) {
+			// Invalid payload
+			http_response_code(400);
+			exit();
+		} catch(\Stripe\Exception\SignatureVerificationException $e) {
+			// Invalid signature
+			http_response_code(400);
+			exit();
+		}
+
+		// Handle the event
+		switch ($event->type) {
+			case 'customer.created':
+				$customer = $event->data->object;
+				$email = sanitize_email( $customer->email );
+				$userdata = array(
+					'user_login'  =>  $email,
+					'user_email' => $email,
+					'role' => 'subscriber'
+				);
+				$user_id = wp_insert_user( $userdata );
+
+				if ( ! is_wp_error( $user_id ) ) {
+					wp_new_user_notification( $user_id, null, 'both');
+					wp_send_json( array(    
+							'user_id' => $user_id,
+							'email' => $email,
+							'password' => '*****',
+							'message' => 'User created successfully.'
+					), 200 );
+				}else{
+					wp_send_json_error( array(  
+							'email' => $email,
+							'name' => $name,
+							'password' => '*****',
+							'error' => 'Unable to create user.',
+					), 500 );
+				}
+
+				break;
+			case 'payment_method.attached':
+				$paymentMethod = $event->data->object; // contains a \Stripe\PaymentMethod
+				// Then define and call a method to handle the successful attachment of a PaymentMethod.
+				// handlePaymentMethodAttached($paymentMethod);
+				break;
+			// ... handle other event types
+			default:
+				// Unexpected event type
+				http_response_code(400);
+				exit();
+		}
+
+		http_response_code(200);
+	}
+}
+
+add_action( 'init', 'servicebot_webhook_listener' );
