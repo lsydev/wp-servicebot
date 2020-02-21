deb09adca86dbab099c9e3572be0896beb52338b
Ka Lung Sy <kalungsy@gmail.com>
2020-02-21 00:51:10 -0500

    adds stripe webhook listener for customer.subscription.created

--- public/class-servicebot-public.php
+++ public/class-servicebot-public.php
@@ -145,6 +145,32 @@ add_action( 'wp_ajax_create_user', 'servicebot_ajax_create_user' );
 add_action( 'wp_ajax_nopriv_create_user', 'servicebot_ajax_create_user' );
 
 
+function servicebot_create_wp_user($customer){
+	$email = sanitize_email( $customer->email );
+	$userdata = array(
+		'user_login'  =>  $email,
+		'user_email' => $email,
+		'role' => 'subscriber'
+	);
+	$user_id = wp_insert_user( $userdata );
+
+	if ( ! is_wp_error( $user_id ) ) {
+		wp_new_user_notification( $user_id, null, 'both');
+		wp_send_json( array(    
+			'user_id' => $user_id,
+			'email' => $email,
+			'password' => '*****',
+			'message' => 'User created successfully.'
+		), 200 );
+	}else{
+		wp_send_json_error( array(  
+			'email' => $email,
+			'name' => $name,
+			'password' => '*****',
+			'error' => 'Unable to create user. The user with this email probably already exists in your Wordpress site.',
+		), 500 );
+	}
+}
 
 function servicebot_webhook_listener() {
 
@@ -163,7 +189,7 @@ function servicebot_webhook_listener() {
 			$stripe_secret_key = get_option('servicebot_servicebot_stripe_live_secret_key_setting');
 		}
 
-		Stripe::setApiKey($stripe_sign_secret);
+		Stripe::setApiKey($stripe_secret_key);
 		$endpoint_secret = $stripe_sign_secret;
 		
 		$payload = @file_get_contents( 'php://input' );
@@ -186,33 +212,25 @@ function servicebot_webhook_listener() {
 
 		// Handle the event
 		switch ($event->type) {
-			case 'customer.created':
-				$customer = $event->data->object;
-				$email = sanitize_email( $customer->email );
-				$userdata = array(
-					'user_login'  =>  $email,
-					'user_email' => $email,
-					'role' => 'subscriber'
-				);
-				$user_id = wp_insert_user( $userdata );
-
-				if ( ! is_wp_error( $user_id ) ) {
-					wp_new_user_notification( $user_id, null, 'both');
-					wp_send_json( array(    
-							'user_id' => $user_id,
-							'email' => $email,
-							'password' => '*****',
-							'message' => 'User created successfully.'
-					), 200 );
+			case 'customer.subscription.created':
+				$subscription = $event->data->object;
+				//check the subscription's product and make sure it's the same as 
+				//the sb_service configured in plugin settings
+				$sb_service = get_option('servicebot_servicebot_service_global_setting');
+				$subscription_sb_service = $subscription['metadata']['sb_service'];
+				if($sb_service == $subscription_sb_service){
+					$customer_id = $subscription->customer;
+					$customer = \Stripe\Customer::retrieve($customer_id);
+					servicebot_create_wp_user($customer);
 				}else{
-					wp_send_json_error( array(  
-							'email' => $email,
-							'name' => $name,
-							'password' => '*****',
-							'error' => 'Unable to create user.',
+					wp_send_json_error( array(
+						"error"=> "Subscription is not created with the sb_service you configured in your Wordpress Servicebot plugin. See Servicebot docs for more info."
 					), 500 );
 				}
-
+				break;
+			case 'customer.created':
+				$customer = $event->data->object;
+				servicebot_create_wp_user($customer);
 				break;
 			case 'payment_method.attached':
 				$paymentMethod = $event->data->object; // contains a \Stripe\PaymentMethod
