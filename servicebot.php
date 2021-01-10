1867d78a93c820822eb7d864906b8b781b4e309a
Ka Lung Sy <kalungsy@gmail.com>
2021-01-10 15:43:26 -0500

    fixes php notices about undefined variables

--- servicebot.php
+++ servicebot.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Servicebot
  * Plugin URI:        http://www.wpexplorer.com/servicebot/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.1.11
+ * Version:           1.1.12
  * Author:            Servicebot
  * Author URI:        https://servicebot.io
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SERVICEBOT_VERSION', '1.1.11' );
+define( 'SERVICEBOT_VERSION', '1.1.12' );
 
 /**
  * The code that runs during plugin activation.
